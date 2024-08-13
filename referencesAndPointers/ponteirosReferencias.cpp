87eb24314ea38334b33de6bc48156be67f6ddb58
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-08-13 14:20:16 -0300

    Update ponteirosReferencias.cpp

--- referencesAndPointers/ponteirosReferencias.cpp
+++ referencesAndPointers/ponteirosReferencias.cpp
@@ -1,42 +1,42 @@
-// //ponteiros e referencias
+// ponteiros e referencias
 
-// //sistema em c++ que traz tudo que aprendemos até agr -> biblioteca 
+// sistema em c++ que traz tudo que aprendemos até agr -> biblioteca 
 
-// // associação -> composição(classe carro/motor) e agregação(obj são partes de outra classe, turma/aluno)
+// associação -> composição(classe carro/motor) e agregação(obj são partes de outra classe, turma/aluno)
 
-// //diagrama de classes, dar uma lida no material 
+// diagrama de classes, dar uma lida no material 
 
-// //smart ponters -> evitar o memory liq 
+// smart ponters -> evitar o memory liq 
 
-// //temos o unique_ptr, shared_ptr e weak_ptr
+// temos o unique_ptr, shared_ptr e weak_ptr
 
-// //std::cin >> x é o método de entrada de dados padrão de c++, é o scanf(x))
+// std::cin >> x é o método de entrada de dados padrão de c++, é o scanf(x))
 
-// #include <iostream>
-// #include <memory> //serve para poder utiliar os smart pointers
+#include <iostream>
+#include <memory> //serve para poder utiliar os smart pointers
 
-// class coisa {
-// int att;
+class coisa {
+int att;
 
-// public:
-// coisa(int i):att(i) {}//qnd começa essa chave, significa que o objeto já existe,  está alterando o atributo do objeto
-//   void fazer_algo() {
-//     std::cout << "coisa::fazer_algo(" << att << ")" << std::endl;
-//   }
-// };
+public:
+coisa(int i):att(i) {}//qnd começa essa chave, significa que o objeto já existe,  está alterando o atributo do objeto
+  void fazer_algo() {
+    std::cout << "coisa::fazer_algo(" << att << ")" << std::endl;
+  }
+};
 
 
-// int main(void) {
-//     std::unique_ptr<coisa> ptr = std::make_unique<coisa>(999); 
+int main(void) {
+    std::unique_ptr<coisa> ptr = std::make_unique<coisa>(999); 
 
-//     ptr ->fazer_algo();
+    ptr ->fazer_algo();
 
-//     std::unique_ptr<coisa> ptr2 = std::move(ptr); 
-//     std::unique_ptr<coisa>ptr2 = ptr; //impossivel de fazer isso, pois o ptr2 já foi movido para o ptr
+    std::unique_ptr<coisa> ptr2 = std::move(ptr); 
+    std::unique_ptr<coisa>ptr2 = ptr; //impossivel de fazer isso, pois o ptr2 já foi movido para o ptr
   
-//     ptr2->fazer_algo();
+    ptr2->fazer_algo();
 
-//     ptr->fazer_algo(); //aq da erro pois ptr aponta para uma posição vazia, null pointer excepted
+    ptr->fazer_algo(); //aq da erro pois ptr aponta para uma posição vazia, null pointer excepted
   
-//   return 0;
-// }
+  return 0;
+}
