6738b6737b1576dedfa7db91e816b442b5a37f4d
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-08-20 11:21:41 -0300

    filesToChange

---
+++ MATRICULA_POO-20241-E002/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+std::cout;
+std::string;
+
+class Pais {
+    string nome_pais
+    string continente;
+
+public:
+    Pais(string p, string c): nome_pais(p), continente(c){}
+
+    void NomePais(string p) {
+        return p
+    }
+    ~Pais() {
+        cout << "País destruído";
+    }
+
+
+};
+
+class Atleta {
+    string nome;
+
+    string modalidade = []
+};
+
+
+
+int main(void) {
+
+
+    return 0;
+}
